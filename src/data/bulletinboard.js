177fc87d40875555560cc00034d47b71c8297ca6
dobahsi <91683644+dobahsi@users.noreply.github.com>
2023-03-07 12:40:45 +0800

    test

--- src/data/bulletinboard.js
+++ src/data/bulletinboard.js
@@ -5,7 +5,7 @@ export const annouceContent = [
     },
     {
         'date': '22/08/18',
-        'text': '2022 CAE暑期實習證書上線囉！',
+        'text': '2022 CAE暑期實習證書上囉！',
     },
     {
         'date': '22/08/18',
